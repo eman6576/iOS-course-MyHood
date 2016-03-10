48a0182187fdcadf5523afdd76f7562ef2852e6d
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-10 03:01:03 -0500

    Created a Cocoa Touch class that manages a cell of the table view and configures the cell.

---
+++ MyHood/PostCell.swift
@@ -0,0 +1,25 @@
+//
+//  PostCell.swift
+//  MyHood
+//
+//  Created by Emanuel  Guerrero on 3/10/16.
+//  Copyright © 2016 Project Omicron. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    @IBOutlet weak var postImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(post: Post) {
+        titleLabel.text = post.title
+        descLabel.text = post.postDesc
+    }
+}
