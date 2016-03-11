a28e7b437186154c80a947ff8e9655061b59ab10
Emanuel  Guerrero <Emanuel@Emanuels-MBP.attlocal.net>
2016-03-10 21:52:01 -0500

    Added functionality for saving a post.

--- MyHood/AddPostVC.swift
+++ MyHood/AddPostVC.swift
@@ -8,26 +8,47 @@
 
 import UIKit
 
-class AddPostVC: UIViewController {
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     @IBOutlet weak var postImage: UIImageView!
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
+    
+    var imagePicker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         postImage.layer.cornerRadius = postImage.frame.size.width / 2
         postImage.clipsToBounds = true
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
     }
     
     @IBAction func addPicButtonTapped(sender: UIButton) {
         sender.setTitle("", forState: .Normal)
+        presentViewController(imagePicker, animated: true, completion: nil)
     }
     
     @IBAction func makePostButtonTapped(sender: UIButton) {
+        if let title = titleTextField.text, let desc = descriptionTextField.text, let image = postImage.image {
+            let imagePath = DataService.instance.saveImageAndCreatePath(image)
+            
+            let post = Post(imagePath: imagePath, title: title, description: desc)
+            
+            DataService.instance.addPost(post)
+            
+            dismissViewControllerAnimated(true, completion: nil)
+        }
     }
     
     @IBAction func cancelButtonTapped(sender: UIButton) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
+        imagePicker.dismissViewControllerAnimated(true, completion: nil)
+        
+        postImage.image = image
     }
 }
